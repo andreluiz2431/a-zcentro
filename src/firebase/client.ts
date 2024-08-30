2ddf6f11d8fd5a029e1cacc083d2de4edc01e9bd
Andre Luiz M. de Abreu <andre.abreu@coop.caal.com.br>
2024-08-30 12:17:40 -0300

    consegui conectar e criar login, bora prox passo

---
+++ src/firebase/client.ts
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAC7u2tfA2oVenv83pKl7YE9eGMWgpNuyc",
+  authDomain: "my-auth-domain",
+  projectId: "test-b51b0",
+};
+
+export const app = initializeApp(firebaseConfig);
