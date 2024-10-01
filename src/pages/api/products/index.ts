eb305a703e5d0c967f7a89412a2005d11d8a2445
Andre Luiz M. de Abreu <andre.abreu@coop.caal.com.br>
2024-10-01 17:55:52 -0300

    Intregração inicial com Bling

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+import axios from 'axios';
+export const GET: APIRoute = async ({ redirect }) => {
+  try {
+    const response = await axios.get(
+      `https://bling.com.br/Api/v2/produtos/json/`,
+      {
+        params: {
+          apikey: import.meta.env.BLING_API_KEY,
+        },
+      }
+    );
+
+    return {
+      body: JSON.stringify(response.data),
+      status: 200,
+    };
+  } catch (error) {
+    return {
+      body: JSON.stringify({ error: 'Erro ao buscar produtos do Bling' }),
+      status: 500,
+    };
+  }
+}
